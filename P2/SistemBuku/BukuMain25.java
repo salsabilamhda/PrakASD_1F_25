9dc87fb8d1a78385d0e23fb7d289c309e9c66103
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2024-02-28 11:45:04 +0700

    P2_percobaan2_Pertanyaan5_1F_25

--- P2/SistemBuku/BukuMain25.java
+++ P2/SistemBuku/BukuMain25.java
@@ -8,12 +8,19 @@ public class BukuMain25 {
         bk1.halaman = 198;
         bk1.stok = 13;
         bk1.harga = 71000;
-        
+
         bk1.tampilInformasi();
         bk1.terjual(5);
         bk1.gantiHarga(60000);
         bk1.tampilInformasi();
 
-       
+       Buku25 bk2 = new Buku25(); //modifikasi
+       bk2.judul = "How to Train Your Dragon";
+       bk2.pengarang = "Cressida Cowell";
+       bk2.halaman = 432;
+       bk2.stok = 0;
+       bk2.harga = 275000;
+
+       bk2.tampilInformasi();
     }
 }
