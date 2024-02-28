4466700b33e9a7124e20bbe16a8922ccc42eb634
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2024-02-28 11:14:47 +0700

    P2_percobaan1_1F_25

---
+++ P2/SistemBuku/Buku25.java
@@ -0,0 +1,38 @@
+package P2.SistemBuku;
+
+public class Buku25 {
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    public Buku25() {
+
+    }
+
+    public Buku25(String jud, String pg, int hal, int stok, int har) {
+        judul = jud;
+        pengarang = pg;
+        halaman = hal;
+        this.stok = stok;
+        harga = har;
+    }
+
+    void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang : "+ pengarang);
+        System.out.println("Jumlah halaman : " + halaman);
+        System.out.println("Sisa stok : " + stok);
+        System.out.println("Harga: Rp " + harga);
+    }
+
+    void terjual(int jml) {
+        stok -= jml;
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+}
