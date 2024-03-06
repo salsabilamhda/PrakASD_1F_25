57a6cf81bff2f5eb412620c0a3e66e3da502fca2
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2024-03-06 13:27:04 +0700

    P3_percobaan3_Pertanyaan2_1F_25

---
+++ P3/ArrayBalok/Segitiga25.java
@@ -0,0 +1,13 @@
+package P3.ArrayBalok;
+
+public class Segitiga25 {
+    public int alas;
+    public int tinggi;
+
+    public Segitiga25(int a, int t)
+    {
+        alas = a;
+        tinggi = t;
+    }
+
+}
