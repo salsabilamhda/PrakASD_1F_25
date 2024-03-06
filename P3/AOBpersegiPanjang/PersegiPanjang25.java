5c3f49a839a2912028c0fffc56ddba66dcad0468
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2024-03-06 11:08:11 +0700

    P3_Percobaan1_1F_25

---
+++ P3/AOBpersegiPanjang/PersegiPanjang25.java
@@ -0,0 +1,27 @@
+package P3.AOBpersegiPanjang;
+
+public class PersegiPanjang25 {
+    
+    public int panjang;
+    public int lebar;
+
+    public static void main(String[] args) {
+        PersegiPanjang25[] ppArray = new PersegiPanjang25[3];
+
+        ppArray[0] = new PersegiPanjang25();
+        ppArray[0] .panjang = 110;
+        ppArray[0] .lebar = 30;
+
+        ppArray[1] = new PersegiPanjang25();
+        ppArray[1] .panjang = 80;
+        ppArray[1] .lebar = 40;
+
+        ppArray[2] = new PersegiPanjang25();
+        ppArray[2] .panjang = 100;
+        ppArray[2] .lebar = 20;
+
+        System.out.println("Persegi Panjang ke-0, panjang: " + ppArray[0].panjang + ", lebar: " + ppArray[0].lebar);
+        System.out.println("Persegi Panjang ke-1, panjang: " + ppArray[1].panjang + ", lebar: " + ppArray[1].lebar);
+        System.out.println("Persegi Panjang ke-2, panjang: " + ppArray[2].panjang + ", lebar: " + ppArray[2].lebar);
+    }
+}
