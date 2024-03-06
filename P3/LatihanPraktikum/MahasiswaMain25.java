fb91e1b2d725dcc78e2d16b870ef73ab21e41d19
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2024-03-06 17:03:39 +0700

    P3_LatihanPraktikum_Pertanyaan2_1F_25

--- P3/LatihanPraktikum/MahasiswaMain25.java
+++ P3/LatihanPraktikum/MahasiswaMain25.java
@@ -6,8 +6,20 @@ public class MahasiswaMain25 {
     public static void main(String[] args) {
         Mahasiswa25[] mhsArray = new Mahasiswa25[3];
         
-        Scanner scan25 = new Scanner(System.in);
 
+        InputData(mhsArray);
+        System.out.println("=====================");
+        tampilData(mhsArray);
+        System.out.println("=====================");
+        hitungRataIPK(mhsArray);
+        System.out.println("=====================");
+        IPKTerbesar(mhsArray);
+
+    }
+
+
+        private static void InputData(Mahasiswa25[] mhsArray) {
+            Scanner scan25 = new Scanner(System.in);
         for(int i = 0; i<mhsArray.length; i++)
         {
             mhsArray[i] = new Mahasiswa25();
@@ -21,11 +33,10 @@ public class MahasiswaMain25 {
             System.out.print("Masukkan IPK mahasiswa: ");
             mhsArray[i].ipk = scan25.nextFloat();
         }
+    }
 
-        scan25.close();
 
-        System.out.println("=======================================");
-
+        private static void tampilData(Mahasiswa25[] mhsArray) {
         for(int i=0; i<mhsArray.length; i++) 
         {
             System.out.println("Data Mahasiswa ke- "+ (i+1));
@@ -35,5 +46,35 @@ public class MahasiswaMain25 {
             System.out.println("IPK Mahasiswa: " + mhsArray[i].ipk);
         }
     }
-    
+
+       private static void hitungRataIPK(Mahasiswa25[] mhsArray) {
+        float totalIPK = 0;
+        for(int i = 0; i<mhsArray.length; i++) {
+            totalIPK += mhsArray[i].ipk / mhsArray.length;
+        }
+        System.out.println("Rata - rata IPK Mahasiswa: " + totalIPK);
+       }
+
+       private static void IPKTerbesar(Mahasiswa25[] mhsArray) {
+        float temp, maxIPK = 0;
+        int indexMhsMaxIPK = 0;
+        for (int i=0; i<mhsArray.length; i++) {
+            if (i==0) {
+                maxIPK = mhsArray[i].ipk;
+                indexMhsMaxIPK = i;
+            } else {
+                temp = mhsArray[i].ipk;
+                if(temp > maxIPK) {
+                    maxIPK = temp;
+                    indexMhsMaxIPK = i;
+                }
+            }
+        }
+        System.out.println("Nilai IPK Terbesar: " + maxIPK);
+        System.out.println(" -Mahasiswa IPK terbesar- ");
+        System.out.println("Nama Mahasiswa: " + mhsArray[indexMhsMaxIPK].namaMhs);
+        System.out.println("NIM Mahasiswa: " + mhsArray[indexMhsMaxIPK].nimMhs);
+        System.out.println("Jenis Kelamin Mahasiswa: " + mhsArray[indexMhsMaxIPK].jenisKelamin);
+        System.out.println("IPK Mahasiswa: " + mhsArray[indexMhsMaxIPK].ipk);
+       }
 }
