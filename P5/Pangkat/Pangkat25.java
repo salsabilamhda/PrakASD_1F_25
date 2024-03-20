af870fe428ca87becb97bcf9ac6ed8b19e9c0f27
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2024-03-20 12:35:08 +0700

    P5_Percobaan2_1F_25

---
+++ P5/Pangkat/Pangkat25.java
@@ -0,0 +1,28 @@
+package P5.Pangkat;
+
+public class Pangkat25 {
+    public int nilai, pangkat;
+
+    int pangkatBF(int a, int n){
+        //int hasil = 0; 
+        int hasil = 1; //perbaikan
+        for(int i=0; i<n; i++){
+            hasil *= a;
+        }
+        return hasil;
+    }
+
+    int pangkatDC(int a, int n){
+        if(n==1){
+           // return 1;
+            return a;  //perbaikan
+        } else{
+            if(n%2==1) // bilangan ganjil
+            {
+                return (pangkatDC(a,n/2)*pangkatDC(a,n/2)*a);
+            }else{
+                return (pangkatDC(a,n/2)*pangkatDC(a,n/2));
+            }
+        }
+    }
+}
