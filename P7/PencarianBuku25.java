4960f065fe2da41c4367aacc1d0cdb2628b40037
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2024-04-03 20:44:44 +0700

    P7_percobaan2_Pertanyaan4_1F_25

--- P7/PencarianBuku25.java
+++ P7/PencarianBuku25.java
@@ -71,7 +71,8 @@ public class PencarianBuku25 {
             mid = (left + right)/2;
             if(cari == listBk[mid].kodeBuku){
                 return(mid);
-            }else if (listBk[mid].kodeBuku > cari){
+            //}else if (listBk[mid].kodeBuku > cari){
+            } else if (listBk[mid].kodeBuku < cari){ //MODFIKASI PERTNYAAN 4
                 return FindBinarySearch(cari, left, mid-1);
             }else {
                 return FindBinarySearch(cari, mid+1, right);
