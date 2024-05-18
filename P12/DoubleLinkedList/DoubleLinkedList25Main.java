cbd9463d4da0f316a950c852a6f4ff31f04c185b
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2024-05-18 11:14:12 +0700

    P12_Percobaan2_1F_25

--- P12/DoubleLinkedList/DoubleLinkedList25Main.java
+++ P12/DoubleLinkedList/DoubleLinkedList25Main.java
@@ -4,19 +4,22 @@ public class DoubleLinkedList25Main {
     public static void main(String[] args) throws Exception {
         DoubleLinkedList25 dll = new DoubleLinkedList25();
         dll.print();
-        System.out.println("Size  : "+dll.size());
-        System.out.println("===============================");
-        dll.addFirst(3);
-        dll.addLast(4);
-        dll.addFirst(7);
+        dll.addLast(50);
+        dll.addLast(40);
+        dll.addLast(10);
+        dll.addLast(20);
         dll.print();
         System.out.println("Size : "+dll.size());
         System.out.println("===============================");
-        dll.add(40, 1);
+        dll.removeFirst();
         dll.print();
-        System.out.println("size : "+dll.size());
+        System.out.println("Size : "+dll.size());
         System.out.println("===============================");
-        dll.clear();
+        dll.removeLast();
+        dll.print();
+        System.out.println("Size : "+dll.size());
+        System.out.println("===============================");
+        dll.remove(1);
         dll.print();
         System.out.println("Size : "+dll.size());
     }   
