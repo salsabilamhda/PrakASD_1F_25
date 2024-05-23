bb347442fc2b33d0e1929aa45cf6ac96b2eb37b5
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2024-05-23 23:21:24 +0700

    P12_Tugas _Pertanyaan2_PushUlang_1F_25

--- P12/TugasPraktikum/Tugas2/Node25.java
+++ P12/TugasPraktikum/Tugas2/Node25.java
@@ -2,14 +2,16 @@ package P12.TugasPraktikum.Tugas2;
 
 public class Node25 {
     int id;
-    String namaPembeli, noHP;
+    String judulFilm;
+    double rating;
 
     Node25 prev, next;
 
-    Node25(Node25 prev, String namaPembeli, String noHP, Node25 next){
+    Node25(Node25 prev, int id, String judulFilm, double rating, Node25 next){
         this.prev = prev;
-        this.namaPembeli = namaPembeli;
-        this.noHP = noHP;
+        this.id = id;
+        this.judulFilm = judulFilm;
+        this.rating = rating;
         this.next = next;
     }
 }
