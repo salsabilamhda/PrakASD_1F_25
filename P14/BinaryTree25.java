92b29b1fcc580eb7339e225f654bf1985cec3697
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2024-05-29 12:40:10 +0700

    P14_Percobaan1_1F_25

---
+++ P14/BinaryTree25.java
@@ -0,0 +1,167 @@
+package P14;
+
+public class BinaryTree25 {
+    Node25 root;
+
+    public BinaryTree25(){
+        root = null;
+    }
+
+    boolean isEmpty(){
+        return root == null;
+    }
+
+    void add(int data){
+        if(isEmpty()){ //tree is empty
+            root = new Node25(data);
+        }else{
+            Node25 current = root;
+            while(true){
+                if(data<current.data){
+                    if(current.left != null){
+                        current = current.left;
+                    }else{
+                        current.left = new Node25(data);
+                        break;
+                    }
+                }else if(data>current.data){
+                    if(current.right != null){
+                        current = current.right;
+                    }else{
+                        current.right = new Node25(data);
+                        break;
+                    }
+                }else{// data is alredy exist
+                    break;
+                }
+            }
+        }
+    }
+
+    boolean find(int data){
+        boolean result = false;
+        Node25 current = root;
+        while(current!=null){
+            if(current.data==data){
+                result = true;
+                break;
+            }else if(data<current.data){
+                current = current.left;
+            }else{
+                current = current.right;
+            }
+        }
+        return result;
+    }
+
+    void traversePreOrder(Node25 node){
+        if(node != null){
+            System.out.print(" " + node.data);
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+    void traversePostOrder(Node25 node){
+        if(node != null){
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            System.out.print(" " + node.data);
+        }
+    }
+    void traverseInOrder(Node25 node){
+        if(node != null){
+            traverseInOrder(node.left);
+            System.out.print(" " + node.data);
+            traverseInOrder(node.right);
+        }
+    }
+
+    Node25 getSuccessor(Node25 del){
+        Node25 successor = del.right;
+        Node25 successorParent = del;
+        while(successor.left!=null){
+            successorParent = successor;
+            successor = successor.left;
+        }
+        if(successor!=del.right){
+            successorParent.left = successor.right;
+            successor.right = del.right;
+        }
+        return successor;
+    }
+
+    void delete(int data){
+        if(isEmpty()){
+            System.out.println("Tree is empty");
+            return;
+        }
+        //find node (current) that will be deleted
+        Node25 parent = root;
+        Node25 current = root;
+        boolean isLeftChild = false;
+        while(current!=null){
+            if(current.data==data){
+                break;
+            }else if(data<current.data){
+                parent = current;
+                current = current.left;
+                isLeftChild = true;
+            }else if(data>current.data){
+                parent = current;
+                current = current.right;
+                isLeftChild = false;
+            }
+        }
+
+            //deletion
+            if(current==null){
+                System.out.println("Couldn't find data!");
+                return;
+            }else{
+                //if there is no child, simply delete it
+                if(current.left==null &&current.right==null){
+                    if(current==root){
+                        root = null;
+                    }else{
+                        if(isLeftChild){
+                            parent.left = null;
+                        }else{
+                            parent.right = null;
+                        }
+                    }
+                }else if(current.left == null){//if there is 1 child(right)
+                    if(current==root){
+                        root = current.right;
+                    }else{
+                        if(isLeftChild){
+                            parent.left = current.right;
+                        }else{
+                            parent.right = current.right;
+                        }
+                    }
+                }else if(current.right == null){//if there is 1 child(left)
+                    if(current==root){
+                        root = current.left;
+                    }else{
+                        if(isLeftChild){
+                            parent.left = current.left;
+                        }else{
+                            parent.right = current.left;
+                        }
+                    }
+                }else{//if there is 2 childs
+                    Node25 successor = getSuccessor(current);
+                    if(current==root){
+                        root = successor;
+                    }else{
+                        if(isLeftChild){
+                            parent.left = successor;
+                        }else{
+                            parent.right = successor;
+                        }
+                        successor.left = current.left;
+                    }
+                }
+            }
+            }
+}
---
+++ P14/Node25.java
@@ -0,0 +1,16 @@
+package P14;
+
+public class Node25 {
+    int data;
+    Node25 left, right;
+
+    public Node25(){
+
+    }
+
+    public Node25(int data){
+        this.left = null;
+        this.data = data;
+        this.right = null;
+    }
+}
