8ee73a0df1cb678622e7e45f7f7c16a8098ee130
Salsabila Mahda Runisha <salsabilamahda217@gmail.com>
2024-06-06 18:26:45 +0700

    P15_LatihanPraktikum_Pertanyaan1_1F_25

--- P15/GraphMain25.java
+++ P15/GraphMain25.java
@@ -1,69 +1,65 @@
 package P15;
 
-//import java.util.Scanner;
+import java.util.Scanner;
 
 public class GraphMain25 {
     public static void main(String[] args) throws Exception {
-        GraphMatriks25 gdg = new GraphMatriks25(4);
-        gdg.makeEdge(0, 1, 50);
-        gdg.makeEdge(1, 0, 60);
-        gdg.makeEdge(1, 2, 70);
-        gdg.makeEdge(2, 1, 80);
-        gdg.makeEdge(2, 3, 40);
-        gdg.makeEdge(3, 0, 90);
-        for (int i = 0; i < 4; i++) { //modifikasi no 4 
-            gdg.degree(i);
-            System.out.println();
-        }
-        gdg.printGraph();
-
-        System.out.println("Hasil setelah penghapusan edge");
-        gdg.removeEdge(2, 1);
-        gdg.printGraph();
-
-
-        /*Scanner sc25 = new Scanner(System.in);
+        Scanner sc25 = new Scanner(System.in);
         Graph25 gedung = new Graph25(6);
 
-        gedung.addEdge(0, 1, 50);
-        gedung.addEdge(0, 2, 100);
-        gedung.addEdge(1, 3, 70);
-        gedung.addEdge(2, 3, 40);
-        gedung.addEdge(3, 4, 60);
-        gedung.addEdge(4, 5, 80);
-        gedung.degree(0);
-        gedung.printGraph();
-
-        //Langkah 17 
-        gedung.removeEdge(1, 3);
-        gedung.printGraph();
-
-        //modifikasi no 5
         while (true) {
             System.out.println("Menu:");
-            System.out.println("1. Cek gedung");
-            System.out.println("2. Keluar");
-            System.out.print("Pilih opsi: ");
+            System.out.println("1. Add Edge");
+            System.out.println("2. Remove Edge");
+            System.out.println("3. Degree");
+            System.out.println("4. Print Graph");
+            System.out.println("5. Check Edge");
+            System.out.println("6. Exit");
+            System.out.print("Choose an option: ");
             int pilihan = sc25.nextInt();
 
-            if (pilihan == 1) {
-                System.out.print("Masukkan gedung asal: ");
-                int asal = sc25.nextInt();
-                System.out.print("Masukkan gedung tujuan: ");
-                int tujuan = sc25.nextInt();
-
-                if (gedung.isConnected(asal, tujuan)) {
-                    System.out.println("Gedung " + (char)('A' + asal) + " dan " + (char)('A' + tujuan) + " bertetangga");
-                } else {
-                    System.out.println("Gedung " + (char)('A' + asal) + " dan " + (char)('A' + tujuan) + " tidak bertetangga");
-                }
-            } else if (pilihan == 2) {
-                break;
-            } else {
+            switch (pilihan) {
+                case 1:
+                    System.out.print("Masukkan gedung asal: ");
+                    int asal = sc25.nextInt();
+                    System.out.print("Masukkan gedung tujuan: ");
+                    int tujuan = sc25.nextInt();
+                    System.out.print("Masukkan jarak: ");
+                    int jarak = sc25.nextInt();
+                    gedung.addEdge(asal, tujuan, jarak);
+                    break;
+                case 2:
+                    System.out.print("Masukkan gedung asal: ");
+                    asal = sc25.nextInt();
+                    System.out.print("Masukkan gedung tujuan: ");
+                    tujuan = sc25.nextInt();
+                    gedung.removeEdge(asal, tujuan);
+                    break;
+                case 3:
+                    System.out.print("Masukkan gedung: ");
+                    asal = sc25.nextInt();
+                    gedung.degree(asal);
+                    break;
+                case 4:
+                    gedung.printGraph();
+                    break;
+                case 5:
+                    System.out.print("Masukkan gedung asal: ");
+                    asal = sc25.nextInt();
+                    System.out.print("Masukkan gedung tujuan: ");
+                    tujuan = sc25.nextInt();
+                    if (gedung.isConnected(asal, tujuan)) {
+                        System.out.println("Gedung " + (char)('A' + asal) + " dan " + (char)('A' + tujuan) + " bertetangga");
+                    } else {
+                        System.out.println("Gedung " + (char)('A' + asal) + " dan " + (char)('A' + tujuan) + " tidak bertetangga");
+                    }
+                    break;
+                case 6:
+                    sc25.close();
+                    return;
+                default:
                 System.out.println("Pilihan tidak valid. Silakan coba lagi.");
             }
         }
-
-        sc25.close();*/
     }
 }
